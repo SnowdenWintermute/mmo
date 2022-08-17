89dc1d5144ef94a5ca512593a198b0de4f21f479
Mike <michael.p.silverman@gmail.com>
2022-08-17 15:20:06 -0400

    border check optimized, cleaned and tested

--- packages/zone-manager/src/determineZoneNeighbors/determineZoneNeighbors.test.ts
+++ packages/zone-manager/src/determineZoneNeighbors/determineZoneNeighbors.test.ts
@@ -2,6 +2,80 @@ import determineZoneNeighbors from "./determineZoneNeighbors";
 import Zone from "@permadeath/zone-node/dist/Zone/Zone";
 import { Point } from "@permadeath/game/dist/base/Point";
 
+test("8 surrounding zones bordering 1 and each other", () => {
+  const zones = {
+    "0": new Zone(0, "192.168.1.1", new Point(10, 10), 10, 10), // self
+    "1": new Zone(1, "192.168.1.2", new Point(20, 0), 10, 10), // northeast
+    "2": new Zone(1, "192.168.1.3", new Point(0, 0), 10, 10), // northwest
+    "3": new Zone(1, "192.168.1.4", new Point(20, 20), 10, 10), // southeast
+    "4": new Zone(1, "192.168.1.5", new Point(0, 20), 10, 10), // southwest
+    "5": new Zone(1, "192.168.1.6", new Point(10, 0), 10, 10), // north
+    "6": new Zone(1, "192.168.1.7", new Point(10, 20), 10, 10), // south
+    "7": new Zone(1, "192.168.1.8", new Point(20, 10), 10, 10), // east
+    "8": new Zone(1, "192.168.1.9", new Point(0, 10), 10, 10), // west
+  };
+  expect(determineZoneNeighbors(zones)).toStrictEqual({
+    "0": {
+      northEast: ["1"],
+      northWest: ["2"],
+      southEast: ["3"],
+      southWest: ["4"],
+      north: ["5"],
+      south: ["6"],
+      east: ["7"],
+      west: ["8"],
+    },
+    "1": {
+      southWest: ["0"],
+      west: ["5"],
+      south: ["7"],
+    },
+    "2": {
+      southEast: ["0"],
+      east: ["5"],
+      south: ["8"],
+    },
+    "3": {
+      northWest: ["0"],
+      north: ["7"],
+      west: ["6"],
+    },
+    "4": {
+      northEast: ["0"],
+      east: ["6"],
+      north: ["8"],
+    },
+    "5": {
+      south: ["0"],
+      west: ["2"],
+      east: ["1"],
+      southEast: ["7"],
+      southWest: ["8"],
+    },
+    "6": {
+      north: ["0"],
+      west: ["4"],
+      east: ["3"],
+      northEast: ["7"],
+      northWest: ["8"],
+    },
+    "7": {
+      west: ["0"],
+      north: ["1"],
+      south: ["3"],
+      northWest: ["5"],
+      southWest: ["6"],
+    },
+    "8": {
+      east: ["0"],
+      south: ["4"],
+      north: ["2"],
+      northEast: ["5"],
+      southEast: ["6"],
+    },
+  });
+});
+
 test("four corners border", () => {
   const zones = {
     "0": new Zone(0, "192.168.1.1", new Point(10, 10), 10, 10), // self
@@ -12,22 +86,22 @@ test("four corners border", () => {
   };
   expect(determineZoneNeighbors(zones)).toStrictEqual({
     "0": {
-      northEast: { "1": { ip: "192.168.1.2" } },
-      northWest: { "2": { ip: "192.168.1.3" } },
-      southEast: { "3": { ip: "192.168.1.4" } },
-      southWest: { "4": { ip: "192.168.1.5" } },
+      northEast: ["1"],
+      northWest: ["2"],
+      southEast: ["3"],
+      southWest: ["4"],
     },
     "1": {
-      southWest: { "0": { ip: "192.168.1.1" } },
+      southWest: ["0"],
     },
     "2": {
-      southEast: { "0": { ip: "192.168.1.1" } },
+      southEast: ["0"],
     },
     "3": {
-      northWest: { "0": { ip: "192.168.1.1" } },
+      northWest: ["0"],
     },
     "4": {
-      northEast: { "0": { ip: "192.168.1.1" } },
+      northEast: ["0"],
     },
   });
 });
@@ -39,10 +113,10 @@ test("northwest corner border", () => {
   };
   expect(determineZoneNeighbors(zones)).toStrictEqual({
     "0": {
-      northWest: { "1": { ip: "192.168.1.2" } },
+      northWest: ["1"],
     },
     "1": {
-      southEast: { "0": { ip: "192.168.1.1" } },
+      southEast: ["0"],
     },
   });
 });
@@ -54,10 +128,10 @@ test("north border directly aligned", () => {
   };
   expect(determineZoneNeighbors(zones)).toStrictEqual({
     "0": {
-      north: { "1": { ip: "192.168.1.2" } },
+      north: ["1"],
     },
     "1": {
-      south: { "0": { ip: "192.168.1.1" } },
+      south: ["0"],
     },
   });
 });
@@ -69,10 +143,10 @@ test("north/south border offset left", () => {
   };
   expect(determineZoneNeighbors(zones)).toStrictEqual({
     "0": {
-      north: { "1": { ip: "192.168.1.2" } },
+      north: ["1"],
     },
     "1": {
-      south: { "0": { ip: "192.168.1.1" } },
+      south: ["0"],
     },
   });
 });
@@ -84,10 +158,10 @@ test("north/south border offset right", () => {
   };
   expect(determineZoneNeighbors(zones)).toStrictEqual({
     "0": {
-      north: { "1": { ip: "192.168.1.2" } },
+      north: ["1"],
     },
     "1": {
-      south: { "0": { ip: "192.168.1.1" } },
+      south: ["0"],
     },
   });
 });
@@ -99,10 +173,10 @@ test("east/west border offset up", () => {
   };
   expect(determineZoneNeighbors(zones)).toStrictEqual({
     "0": {
-      east: { "1": { ip: "192.168.1.2" } },
+      east: ["1"],
     },
     "1": {
-      west: { "0": { ip: "192.168.1.1" } },
+      west: ["0"],
     },
   });
 });
@@ -113,10 +187,31 @@ test("east/west border offset down", () => {
   };
   expect(determineZoneNeighbors(zones)).toStrictEqual({
     "0": {
-      east: { "1": { ip: "192.168.1.2" } },
+      east: ["1"],
     },
     "1": {
-      west: { "0": { ip: "192.168.1.1" } },
+      west: ["0"],
+    },
+  });
+});
+
+test("two zones bordering to east", () => {
+  const zones = {
+    "0": new Zone(0, "192.168.1.1", new Point(10, 10), 10, 10),
+    "1": new Zone(1, "192.168.1.2", new Point(20, 15), 10, 10),
+    "2": new Zone(1, "192.168.1.3", new Point(20, 5), 10, 10),
+  };
+  expect(determineZoneNeighbors(zones)).toStrictEqual({
+    "0": {
+      east: ["1", "2"],
+    },
+    "1": {
+      west: ["0"],
+      north: ["2"],
+    },
+    "2": {
+      west: ["0"],
+      south: ["1"],
     },
   });
 });
--- packages/zone-manager/src/determineZoneNeighbors/determineZoneNeighbors.ts
+++ packages/zone-manager/src/determineZoneNeighbors/determineZoneNeighbors.ts
@@ -1,128 +1,60 @@
 import { Rectangle } from "@permadeath/game/dist/base/Rectangle";
 import Zone from "@permadeath/zone-node/dist/Zone/Zone";
-import { between } from "@permadeath/utils/dist/index";
-
-enum Border {
-  north = "north",
-  south = "south",
-  east = "east",
-  west = "west",
-  northEast = "northEast",
-  northWest = "northWest",
-  southEast = "southEast",
-  southWest = "southWest",
-}
-
-const borderOpposites = {
-  north: Border.south,
-  south: Border.north,
-  east: Border.west,
-  west: Border.east,
-  northEast: Border.southWest,
-  southWest: Border.northEast,
-  northWest: Border.southEast,
-  southEast: Border.northWest,
-};
+import rectangleBorderingDirection, { BorderDirection } from "./rectangleBorderingDirection";
 
 interface ZoneNeighborList {
   [id: string]: {
-    north?: { [id: string]: { ip: string } } | null;
-    south?: { [id: string]: { ip: string } } | null;
-    east?: { [id: string]: { ip: string } } | null;
-    west?: { [id: string]: { ip: string } } | null;
-    northEast?: { [id: string]: { ip: string } } | null;
-    northWest?: { [id: string]: { ip: string } } | null;
-    southEast?: { [id: string]: { ip: string } } | null;
-    southWest?: { [id: string]: { ip: string } } | null;
+    north?: string[];
+    south?: string[];
+    east?: string[];
+    west?: string[];
+    northEast?: string[];
+    northWest?: string[];
+    southEast?: string[];
+    southWest?: string[];
   };
 }
+
+const borderOpposites = {
+  north: BorderDirection.south,
+  south: BorderDirection.north,
+  east: BorderDirection.west,
+  west: BorderDirection.east,
+  northEast: BorderDirection.southWest,
+  southWest: BorderDirection.northEast,
+  northWest: BorderDirection.southEast,
+  southEast: BorderDirection.northWest,
+};
 
 export default function determineZoneNeighbors(zones: { [key: string]: Zone }) {
   const zoneNeighborList: ZoneNeighborList = {};
   const zonesAlreadyFullyCompared: string[] = [];
+
   for (const zoneId in zones) {
     const zone = zones[zoneId];
-    zoneNeighborList[zoneId] = {};
+    if (!zoneNeighborList.hasOwnProperty(zoneId)) zoneNeighborList[zoneId] = {};
     const territory = zone.territory.current;
     const currRect = new Rectangle(territory.origin, territory.width, territory.height);
-    console.log(zonesAlreadyFullyCompared);
+
     for (const otherZoneId in zones) {
-      let border: Border | null = null;
+      if (otherZoneId === zoneId || zonesAlreadyFullyCompared.includes(otherZoneId)) continue;
       const otherZone = zones[otherZoneId];
-      if (otherZoneId !== zoneId && !zonesAlreadyFullyCompared.includes(otherZoneId)) {
-        const comparingTerritory = otherZone.territory.current;
-        const otherRect = new Rectangle(comparingTerritory.origin, comparingTerritory.width, comparingTerritory.height);
-        if (
-          currRect.origin.y - 1 === otherRect.bottomY &&
-          (between(currRect.origin.x, otherRect.bottomLeftCorner.x, otherRect.bottomRightCorner.x) ||
-            between(currRect.topRightCorner.x, otherRect.bottomLeftCorner.x, otherRect.bottomRightCorner.x))
-        )
-          border = Border.north;
-        else if (
-          currRect.bottomY + 1 === otherRect.topY &&
-          (between(currRect.bottomLeftCorner.x, otherRect.origin.x, otherRect.topRightCorner.x) ||
-            between(currRect.bottomRightCorner.x, otherRect.origin.x, otherRect.topRightCorner.x))
-        )
-          border = Border.south;
-        else if (
-          currRect.rightX + 1 === otherRect.leftX &&
-          (between(currRect.topRightCorner.y, otherRect.origin.y, otherRect.bottomLeftCorner.y) ||
-            between(currRect.bottomRightCorner.y, otherRect.origin.y, otherRect.bottomLeftCorner.y))
-        )
-          border = Border.east;
-        else if (
-          currRect.leftX - 1 === otherRect.rightX &&
-          (between(currRect.origin.y, otherRect.topRightCorner.y, otherRect.bottomRightCorner.y) ||
-            between(currRect.bottomLeftCorner.y, otherRect.topRightCorner.y, otherRect.bottomRightCorner.y))
-        )
-          border = Border.west;
-        else if (
-          currRect.topRightCorner.x + 1 === otherRect.bottomLeftCorner.x &&
-          currRect.topRightCorner.y - 1 === otherRect.bottomLeftCorner.y
-        )
-          border = Border.northEast;
-        else if (
-          currRect.origin.x - 1 === otherRect.bottomRightCorner.x &&
-          currRect.origin.y - 1 === otherRect.bottomRightCorner.y
-        )
-          border = Border.northWest;
-        else if (
-          currRect.bottomRightCorner.x + 1 === otherRect.origin.x &&
-          currRect.bottomRightCorner.y + 1 === otherRect.origin.y
-        )
-          border = Border.southEast;
-        else if (
-          currRect.bottomLeftCorner.x - 1 === otherRect.topRightCorner.x &&
-          currRect.bottomLeftCorner.y + 1 === otherRect.topRightCorner.y
-        )
-          border = Border.southWest;
-      }
-      // console.log("border: " + border);
-      if (border) {
-        if (zoneNeighborList[zoneId]?.hasOwnProperty(border))
-          zoneNeighborList[zoneId][border] = {
-            ...zoneNeighborList[zoneId][border],
-            [otherZoneId]: { ip: otherZone.ip },
-          };
-        else
-          zoneNeighborList[zoneId][border] = {
-            [otherZoneId]: { ip: otherZone.ip },
-          };
-        const oppositeBorder = borderOpposites[border];
-        if (!zoneNeighborList[otherZoneId]) zoneNeighborList[otherZoneId] = {};
-        if (zoneNeighborList[otherZoneId]?.hasOwnProperty(oppositeBorder))
-          zoneNeighborList[otherZoneId][oppositeBorder] = {
-            ...zoneNeighborList[otherZoneId][oppositeBorder],
-            [zoneId]: { ip: zone.ip },
-          };
-        else
-          zoneNeighborList[otherZoneId][oppositeBorder] = {
-            [zoneId]: { ip: zone.ip },
-          };
-      }
+      const comparingTerritory = otherZone.territory.current;
+      const otherRect = new Rectangle(comparingTerritory.origin, comparingTerritory.width, comparingTerritory.height);
+      const border = rectangleBorderingDirection(currRect, otherRect);
+      if (!border) continue;
+
+      if (zoneNeighborList[zoneId].hasOwnProperty(border)) zoneNeighborList[zoneId][border]?.push(otherZoneId);
+      else zoneNeighborList[zoneId][border] = [otherZoneId];
+
+      const oppositeBorder = borderOpposites[border];
+      if (!zoneNeighborList.hasOwnProperty(otherZoneId)) zoneNeighborList[otherZoneId] = {};
+      if (zoneNeighborList[otherZoneId].hasOwnProperty(oppositeBorder))
+        zoneNeighborList[otherZoneId][oppositeBorder]?.push(zoneId);
+      else zoneNeighborList[otherZoneId][oppositeBorder] = [zoneId];
     }
     zonesAlreadyFullyCompared.push(zoneId);
   }
-  // console.log(zoneNeighborList);
+
   return zoneNeighborList;
 }
