2d16c937796ca83e9f978d6c6c123e12e3c0c1ed
Mike <michael.p.silverman@gmail.com>
2022-08-27 08:57:25 -0400

    behaviors and actions

--- packages/game/src/entities/BehavioralEntity.ts
+++ packages/game/src/entities/BehavioralEntity.ts
@@ -4,7 +4,6 @@ import Action from "./actions/Action";
 import Behavior from "./behaviors/Behavior";
 
 export default class BehavioralEntity extends Entity {
-  behaviors: Behavior[];
   constructor(
     id: string,
     name: string,
--- packages/game/src/entities/actions/Action.ts
+++ packages/game/src/entities/actions/Action.ts
@@ -1,29 +1,23 @@
 export default class Action {
   requirements: boolean[];
   effect: () => void;
-  steps?: {
-    current: number;
-    total: number;
-  };
+  duration?: number;
+  timeInitiated: number | null;
   onInitiated?: () => void;
   onCancelled?: () => void;
   onCompleted?: () => void;
   constructor(
     requirements: boolean[],
     effect: () => void,
-    steps?: number,
+    duration?: number,
     onInitiated?: () => void,
     onCancelled?: () => void,
     onCompleted?: () => void
   ) {
     this.requirements = requirements;
     this.effect = effect;
-    this.steps = steps
-      ? {
-          current: steps,
-          total: steps,
-        }
-      : undefined;
+    this.duration = duration ? duration : undefined;
+    this.timeInitiated = null;
     this.onInitiated = onInitiated;
     this.onCancelled = onCancelled;
     this.onCompleted = onCompleted;
